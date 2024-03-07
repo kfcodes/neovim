2b4346bfb59da8042329f804d8f3152e78537166
kfcodes <87446283+kfcodes@users.noreply.github.com>
2024-03-07 16:37:38 +0000

    Updated the import paths to the new folder structure

--- init.lua
+++ init.lua
@@ -87,23 +87,23 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Set to true if you have a Nerd Font installed
 vim.g.have_nerd_font = false
 
 -- [[ Setting options ]]
-require 'options'
+require("config.options")
 
 -- [[ Basic Keymaps ]]
-require 'keymaps'
+require("config.keymaps")
 
 -- [[ Install `lazy.nvim` plugin manager ]]
-require 'lazy-bootstrap'
+require("config.lazy-bootstrap")
 
 -- [[ Configure and install plugins ]]
-require 'lazy-plugins'
+require("config.lazy-plugins")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
