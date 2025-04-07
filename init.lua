67a110bcac780c4c1377672b845f384b598778c3
kfcodes <+kfcodes@users.noreply.github.com>
2025-04-07 12:09:04 +0100

    Working version of the updated structure

--- init.lua
+++ init.lua
@@ -1,23 +1,4 @@
--- Bootstrap Lazy.nvim (Plugin Manager)
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", lazypath
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- Load Core Settings (Options, Keymaps, Autocommands)
-require("core")
-require("lazy").setup("plugins")
-
-require("lazy").setup("plugins", {
-    checker = {
-    enabled = true,
-    notify = false,
-  },
-  change_detection = {
-    notify = false,
-  },
-})
+require("config.core")
+require("config.lazy")
+--.setup("config.plugins")
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim","
+    "folke/todo-comments.nvim",
   },
   config = function()
     local telescope = require("telescope")
